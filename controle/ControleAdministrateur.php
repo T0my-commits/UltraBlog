23e2096bba554b5ba90211f3590c225259c2d095
Thomas D <thomas.DEVIENNE@etu.uca.fr>
2021-11-24 19:04:23 +0100

    Ajout template de controle (voir controle/ControleAdministrateur.php version 7)

---
+++ controle/ControleAdministrateur.php
@@ -0,0 +1,61 @@
+<?php
+
+class ControleAdministrateur {
+
+	/**
+	 * Constructeur de la classe
+	*/
+	function __construct() {
+		try
+		{
+			/* On veut gérer les actions suivantes :
+			 *   - systeme de pagination
+			 *   - addnews (l'administrateur veut ajouter une news)
+			 *   - inscription
+			 *   - connexion
+			 * 
+			 * Ces actions sont passées dans l'URL via une méthode GET.
+			 * Lorsque que nous traiterons de données plus confidentielles, nous communiquerons ces données
+			 * via une méthode POST (exemple : login, mot de passe)
+			*/
+
+			$action = $_GET["action"];
+
+			switch ($action) {
+				case NULL:
+					// action
+					break;
+
+				case "page":
+					// on appelle les bonnes pages
+					break;
+
+				case "addnews":
+					// on redirige vers la page d'ajout de news
+					break;
+
+				case "inscription":
+					// on redirige vers la page d'inscription
+					break;
+
+				case "connexion":
+					// on redirige vers la page de connexion
+					break;
+
+				default:
+					// page erreur;
+					break;
+			}
+		}
+		catch (PDOException $e)
+		{
+			echo $e->getMessage();
+		}
+		catch (Exception $e)
+		{
+			echo $e->getMessage();
+		}
+	}
+}
+
+?>
